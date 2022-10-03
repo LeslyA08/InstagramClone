5437eb8bd53bef732bb2bdb59f5f5d1a4f3f2d11
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-10-03 09:50:25 -0500

    initial commit, added some pointers in readme, added a profile

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # InstagramClone
+
+-make data file with profile information:
+profile name
+profile picture
+follow button
+posts, followers, following
+summary paragraph
+circle pictures mimicking reels
+array of photos
+search by username
+have different routs to different users
---
+++ instagram-clone-project/src/data/profiles.js
@@ -0,0 +1,24 @@
+const profiles = [
+    {
+        "profile_name" : "Garak",
+        "profile_photo": "https://pets.wilco.org/portals/1/2020/NewFront/augieCircle.png",
+        "posts" : "300",
+        "followers": "10.10m",
+        "following": "1",
+        "summary_title": "Elim Garak the Cat",
+        "summary":"The official Garak the Cat page. Not a spy.",
+        "posts": [
+                "https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=%5B1120%2C599%5D&w=2000&h=1333&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2020%2F08%2F07%2Fgray-tabby-american-shorthair_757090435-1-2000.jpg",
+                "https://s36537.pcdn.co/wp-content/uploads/2018/09/A-gray-tabby-cat-with-green-eyes-close-up.jpg.optimal.jpg",
+                "https://www.bubblypet.com/wp-content/uploads/2021/08/Gray-tabby-cat-with-M-on-its-forehead.jpg",
+                "https://image.shutterstock.com/image-photo/gray-striped-cat-lying-on-260nw-679796272.jpg",
+                "https://cdn.pixabay.com/photo/2020/09/04/10/07/cat-5543572_960_720.jpg",
+                "https://i1.pickpik.com/photos/110/549/129/cat-tomcat-animal-portrait-preview.jpg",
+                "https://pixfeeds.com/images/cats/1280-174645515-tabby-kitten.jpg",
+                "https://i.pinimg.com/564x/50/ab/31/50ab31d3659151f1bb8b5774da801241.jpg",
+                "https://thumbs.dreamstime.com/z/beautiful-grey-tabby-cat-sitting-autumn-leaves-yellow-day-161293656.jpg"
+        ]
+
+
+    }
+]
